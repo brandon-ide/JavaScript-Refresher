33e3825d0237248b59a3a53854b7a99eaf24178d
Brandon Ide <brandonide@Brandons-MacBook-Air.local>
2024-08-19 22:33:31 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstName = "Brandon";
+let lastName = "Ide";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+let population = 7768;
+
+console.log(population);
+
+console.log(population * 3);
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "violet"];
+
+for (let i = 1; i < 6; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
